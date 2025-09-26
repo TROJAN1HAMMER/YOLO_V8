b72e9359176a3e3ef056c807a5fadf2ecfbbac79
TROJAN1HAMMER <balaachuanu@gmail.com>
2025-09-26 18:40:18 +0530

    updated the backend link with nginx activation

--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -127,7 +127,7 @@ function UploadForm() {
       formData.append("file", image);
 
       // send to backend
-      const response = await axios.post("http://13.218.84.170:8000/predict/", formData);
+      const response = await axios.post("https://13-218-84-170.sslip.io/predict/", formData);
       const { detections } = response.data;
 
       // create watermarked data url from original file (so local watermark is applied)
